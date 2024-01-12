cedc4ad3cd306fa626585738abec5d3d1d7df5e5
mcooiedo <mcooiedo@126.com>
2024-01-12 11:40:13 +0800

    Update test.sh

--- pltree/test.sh
+++ pltree/test.sh
@@ -2,7 +2,5 @@
 ./run.sh /home/zzg/data/uniform_dense_200M_uint64 binary uint64_t 100000000 200000000 mixed 0 1 0 1 >>Pal.txt
 ./run.sh /home/zzg/data/osm_cellids_200M_uint64 binary uint64_t 100000000 200000000 mixed 0 1 0 1 >>Pal.txt
 ./run.sh /home/zzg/data/fb_200M_uint64 binary uint64_t 100000000 200000000 mixed 0 1 0 1 >>Pal.txt
-./run.sh /home/zzg/data/uniform_dense_200M_uint64 binary uint64_t 100000000 200000000 mixed 0  1 1 1 >>Pal.txt
-./run.sh /home/zzg/data/normal_200M_uint64 binary uint64_t 100000000 200000000 mixed 0 1 1 1 >>Pal.txt
-./run.sh /home/zzg/data/osm_cellids_200M_uint64 binary uint64_t 100000000 200000000 mixed 0  1 1 1 >>Pal.txt
-./run.sh /home/zzg/data/fb_200M_uint64 binary uint64_t 100000000 200000000 mixed 0 1 1 1 >>Pal.txt
+./run.sh /home/zzg/data/genome binary uint64_t 100000000 200000000 mixed 0  1 1 1 >>Pal.txt
+./run.sh /home/zzg/data/planet binary uint64_t 100000000 200000000 mixed 0 1 1 1 >>Pal.txt
